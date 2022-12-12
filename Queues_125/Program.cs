564eaf0a8be19b8eca87ce32f924601ee168af59
riccoarisdy <riccoarisdy@gmail.com>
2022-12-12 16:41:27 +0700

    add namespace, add nitializing the values of the variables REAR and FRONT to -1 to public program and class program

--- Queues_125/Program.cs
+++ Queues_125/Program.cs
@@ -6,13 +6,20 @@ using System.Threading.Tasks;
 
 namespace Queues_125
 {
-    internal class Program
+    class Program
     {
         int FRONT, REAR, max = 5;
         int[] queues_array = new int[5];
+
+        public Program()
+        {
+            /*initializing the values of the variables REAR and FRONT to -1 to idicate that
+             *  the queues is initialy empty.*/
+            FRONT = -1;
+            REAR = -1;
+        }
         static void Main(string[] args)
         {
-
         }
     }
 }
