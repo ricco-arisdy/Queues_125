35d26525bc136e55d74ef8e9eb5c059f3a665ba3
riccoarisdy <riccoarisdy@gmail.com>
2022-12-12 17:17:15 +0700

    add  element to be deleted is at the last position of the array, add Check if the queue has one element to method remove

--- Queues_125/Program.cs
+++ Queues_125/Program.cs
@@ -54,6 +54,24 @@ namespace Queues_125
                 Console.WriteLine("Queue underflow\n");
                 return;
             }
+            Console.WriteLine("\n The element deleted from the queue is: " + queues_array[FRONT] + "\n");
+            
+            /*Check if the queue has one element.*/
+            if (FRONT == REAR)
+            {
+                FRONT = -1;
+                REAR = -1;
+            }
+            else
+            {
+                /* if the element to be deleted is at the last position of the array, then the value
+                 * of FRONT is set to 0 i.e to the first element of the array. */
+                if (FRONT == max - 1)
+                    FRONT = 0;
+                else
+                    /*FRONT is incremented by one if it is not the first element of array. */
+                    FRONT = FRONT + 1;
+            }
         }
         static void Main(string[] args)
         {
