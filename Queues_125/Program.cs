9e7df733b69c6859acb121b19e9526995176a323
riccoarisdy <riccoarisdy@gmail.com>
2022-12-12 16:34:38 +0700

    Add project files.

---
+++ Queues_125/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues_125
+{
+    internal class Program
+    {
+        int FRONT, REAR, max = 5;
+        int[] queues_array = new int[5];
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
